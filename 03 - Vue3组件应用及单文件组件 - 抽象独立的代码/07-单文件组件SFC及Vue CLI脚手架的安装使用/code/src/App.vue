109c04f5432c3979b851ad63a2ca00bece5dbce9
CodeCan <137749272@qq.com>
2024-04-17 21:54:12 +0800

    chore:unzip file

---
+++ 03 - Vue3组件应用及单文件组件 - 抽象独立的代码/07-单文件组件SFC及Vue CLI脚手架的安装使用/code/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <img alt="Vue logo" :src="src">
+  <h1 class="title">根组件的title</h1>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import logo from './assets/logo.png'
+import './assets/common.css'
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      src: logo
+    }
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
